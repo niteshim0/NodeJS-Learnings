fb47518ba46772bab2a04f3a823018a89344944b
niteshim0 <niteshim111@gmail.com>
2023-09-20 00:16:35 +0530

    modules uploaded

---
+++ Modules/example.js
@@ -0,0 +1,3 @@
+const greet = require('./myModule');
+
+console.log(greet('Nitesh')); // Outputs: Hello, John
---
+++ Modules/greet.js
@@ -0,0 +1,7 @@
+// myModule.js
+const greeting = "Hello,how is it like to learn Node.js ";
+
+function greet(name) {
+  return greeting + name;
+}
+module.exports = greet;
